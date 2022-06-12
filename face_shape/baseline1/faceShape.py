343fe5ba9a86521dac66bc07d0aba65e05328e0e
lon5948 <a0988282303@gmail.com>
2022-06-13 02:24:57 +0800

    test in main is ok

--- face_shape/baseline1/faceShape.py
+++ face_shape/baseline1/faceShape.py
@@ -4,37 +4,45 @@ import face_recognition
 import math
 
 def shape(chin):
-    a = math.sqrt(abs(chin[6][0]-chin[6][0])**2+abs(chin[6][1]-chin[8][1])**2)
-    b = math.sqrt(abs(chin[8][0]-chin[10][0])**2+abs(chin[8][1]-chin[10][1])**2)
-    c = math.sqrt(abs(chin[6][0]-chin[10][0])**2+abs(chin[6][1]-chin[10][1])**2)
+    a = math.sqrt(abs(chin[7][0]-chin[8][0])**2+abs(chin[7][1]-chin[8][1])**2)
+    b = math.sqrt(abs(chin[8][0]-chin[9][0])**2+abs(chin[8][1]-chin[9][1])**2)
+    c = math.sqrt(abs(chin[7][0]-chin[9][0])**2+abs(chin[7][1]-chin[9][1])**2)
     cos = (a**2+b**2-c**2)/(2*a*b)
     angle1 = math.acos(cos)*180/math.pi
-    if angle1 > 150:
-        print('sqrare')
+    if angle1 > 163:
         return('square')
     a = math.sqrt(abs(chin[4][0]-chin[8][0])**2+abs(chin[4][1]-chin[8][1])**2)
     b = math.sqrt(abs(chin[8][0]-chin[12][0])**2+abs(chin[8][1]-chin[12][1])**2)
     c = math.sqrt(abs(chin[4][0]-chin[12][0])**2+abs(chin[4][1]-chin[12][1])**2)
     cos = (a**2+b**2-c**2)/(2*a*b)
     angle2 = math.acos(cos)*180/math.pi
-    print(angle2)
-    if angle1 < 100:
-        print('triangle')
+    if angle2 < 105:
         return('triangle')
     else:
-        print('round')
         return('round')
 
-image = face_recognition.load_image_file('./test2.jpeg')
-face_landmarks = face_recognition.face_landmarks(image)[0]
+hit = 0
+index = 0
+length = len(os.listdir('C:/Users/user/LAIAIAI/face_shape/datasets/round'))
+for filename in os.listdir('C:/Users/user/LAIAIAI/face_shape/datasets/round'):
+    index += 1
+    if index == 10:
+        break
+    image = face_recognition.load_image_file(os.path.join('C:/Users/user/LAIAIAI/face_shape/datasets/round',filename))
+    face_landmarks = face_recognition.face_landmarks(image)[0]
+    '''
+    # draw the line of the chin
+    pil_image = Image.fromarray(image)
+    d = ImageDraw.Draw(pil_image)
+    d.line(face_landmarks['chin'], width=10)
+    pil_image.save('output2.jpg')
+    '''
+    face = shape(face_landmarks['chin'])
+    if face == 'round':
+        hit += 1
 
-pil_image = Image.fromarray(image)
-d = ImageDraw.Draw(pil_image)
-d.line(face_landmarks['chin'], width=10)
-pil_image.save('output2.jpg')
-
-face = shape(face_landmarks['chin'])
-print(face)
+print('length:',length)
+print('accuracy: ',hit/length)
 
 
 
